aed182bcc44db6a26c37ac5a6b5440ee398ff23a
spuerta <puertasantiago2002@gmail.com>
2025-09-26 02:18:50 -0500

    feat: Added mock data to test the app

---
+++ src/db/scripts/03_populate_tables.sql
@@ -0,0 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+\c event_access;
+
+INSERT INTO users (username, password_hash)
+VALUES 
+    ('spuertaf', 'spuertaf'),
+    ('juanperez', 'juanperez');
+
+INSERT INTO tickets (seat, gate, seed, status)
+VALUES
+    ('A12', 'G1', gen_random_bytes(20), 'valid'),
+    ('B05', 'G2', gen_random_bytes(20), 'valid'),
+    ('C18', 'G3', gen_random_bytes(20), 'valid'),
+    ('D10', 'G1', gen_random_bytes(20), 'valid'),
+    ('E20', 'G2', gen_random_bytes(20), 'valid');
