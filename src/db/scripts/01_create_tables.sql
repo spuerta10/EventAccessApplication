c87c97c02a11d0ce03be8e799498068b44b9842e
spuerta <puertasantiago2002@gmail.com>
2025-09-26 02:18:09 -0500

    feat: Added Script to create users and tickets tables

---
+++ src/db/scripts/01_create_tables.sql
@@ -0,0 +1,19 @@
+\c event_access;
+
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE tickets (
+    ticket_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NULL REFERENCES users(user_id),  -- not given user until registered
+    seat VARCHAR(10) NOT NULL,
+    gate VARCHAR(10) NOT NULL,
+    seed BYTEA NOT NULL,                          -- TOTP seed
+    status VARCHAR(20) NOT NULL DEFAULT 'valid',
+    created_at TIMESTAMP DEFAULT now(),
+    used_at TIMESTAMP,
+    CONSTRAINT uq_ticket_seat_gate UNIQUE (seat, gate)  -- unique seat + gate convination
+);
