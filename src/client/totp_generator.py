b6fefb782ac473e5d2d40da7bf67017c60bb7a5a
spuerta <puertasantiago2002@gmail.com>
2025-09-26 02:17:36 -0500

    feat: Added client app TOTP generator with b64 seed as input

---
+++ src/client/totp_generator.py
@@ -0,0 +1,26 @@
+from typing import ClassVar
+from base64 import b64decode, b32encode
+from argparse import ArgumentParser
+
+from pyotp import TOTP
+
+class TOTPGenerator:
+    TOTP_INTERVAL_SECONDS: ClassVar[int] = 60
+    
+    def __init__(self, seed_base64: str) -> None:
+        bytes_seed: bytes = b64decode(seed_base64)
+        seed_base32 = b32encode(bytes_seed).decode("utf-8")
+        self.__totp = TOTP(seed_base32, interval=self.TOTP_INTERVAL_SECONDS)
+    
+    def generate_code(self) -> str:
+        return self.__totp.now()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Genera un código TOTP desde una semilla hexadecimal")
+    parser.add_argument("seed", type=str, help="Seed en hexadecimal (sin 0x)")
+    args = parser.parse_args()
+
+    totp = TOTPGenerator(args.seed)
+    code: str = totp.generate_code()
+    print(f"Your TOTP code is: {code}")
