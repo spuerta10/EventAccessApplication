6125330e16ccf86e0e583c80fcd5447dc4b24012
spuerta <puertasantiago2002@gmail.com>
2025-09-26 02:19:43 -0500

    feat: Added app entrypoint

---
+++ src/register_ticket_api/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+from infraestructure import PostgreSQLDbContext
+from repositories import TicketRepository, UserRepository
+from services import TicketService
+from controllers import TicketsController
+
+app = FastAPI()
+
+psql_context = PostgreSQLDbContext()
+user_repo = UserRepository(psql_context)
+ticket_repo = TicketRepository(db_context=psql_context)
+ticket_service = TicketService(user_repo=user_repo, ticket_repo=ticket_repo)
+tickets_controller = TicketsController(ticket_service=ticket_service)
+
+app.include_router(tickets_controller.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
