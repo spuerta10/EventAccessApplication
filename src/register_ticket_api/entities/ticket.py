315ea9f93c9285c79c316668200fb3616ea44991
spuerta <puertasantiago2002@gmail.com>
2025-10-10 23:07:56 -0500

    style: Improved file style with pre-commit

--- src/register_ticket_api/entities/ticket.py
+++ src/register_ticket_api/entities/ticket.py
@@ -1,15 +1,16 @@
-from typing import Optional, Literal
-from uuid import UUID
 from datetime import datetime
+from typing import Literal
+from uuid import UUID
 
 from pydantic import BaseModel
 
+
 class Ticket(BaseModel):
-    id: Optional[UUID] = None
-    user_id: Optional[UUID] = None
+    id: UUID | None = None
+    user_id: UUID | None = None
     seat: str
     gate: str
-    seed: Optional[str] = None  # TODO: should be secretstr
+    seed: str | None = None  # TODO: should be secretstr
     status: Literal["valid", "used", "revoked"] = "valid"
-    created_at: Optional[datetime] = None
-    used_at: Optional[datetime] = None
+    created_at: datetime | None = None
+    used_at: datetime | None = None
