2d2f150079f04f7db94e7e15b735c8e01599cef3
spuerta <puertasantiago2002@gmail.com>
2025-10-10 23:10:30 -0500

    style: Improved file style with pre-commit

--- src/register_ticket_api/interfaces/i_ticket_repository.py
+++ src/register_ticket_api/interfaces/i_ticket_repository.py
@@ -1,17 +1,16 @@
 from abc import ABC, abstractmethod
-from typing import Optional
 from uuid import UUID
 
-from entities import User, Ticket
+from entities import Ticket, User
+
 
 class ITicketRepository(ABC):
-    
     @abstractmethod
     def register_ticket(self, user: User, ticket: Ticket) -> bool:
         pass
 
     @abstractmethod
-    def get_by_ticket_details(self, seat: str, gate: str) -> Optional[Ticket]:
+    def get_by_ticket_details(self, seat: str, gate: str) -> Ticket | None:
         # TODO: In the long run this will fail due to the abscence of an event entity
         pass
 
--- src/register_ticket_api/interfaces/i_user_repository.py
+++ src/register_ticket_api/interfaces/i_user_repository.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import Optional
 
 from entities import User
+
 
 class IUserRepository(ABC):
     @abstractmethod
     def create_user(self, new_user: User) -> bool:
         pass
 
-    def get_by_username(self, username: str) -> Optional[User]:
+    @abstractmethod
+    def get_by_username(self, username: str) -> User | None:
         pass
