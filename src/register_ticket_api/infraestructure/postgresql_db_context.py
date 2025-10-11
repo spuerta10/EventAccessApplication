89164584318556fc8987b15236880eef5ba2f6d7
spuerta <puertasantiago2002@gmail.com>
2025-10-10 23:13:06 -0500

    style: Improved file style with pre-commit

--- src/register_ticket_api/infraestructure/postgresql_db_context.py
+++ src/register_ticket_api/infraestructure/postgresql_db_context.py
@@ -1,20 +1,21 @@
 import os
 
-from dotenv import load_dotenv
 import asyncpg
+from dotenv import load_dotenv
 
 load_dotenv()  # load env variables from .env file
+
 
 class PostgreSQLDbContext:
     def __parse_env_vars(self) -> dict:
         return {
-            'host': os.getenv('DB_HOST'),
-            'port': int(os.getenv('DB_PORT')),
-            'user': os.getenv('DB_USER'),
-            'password': os.getenv('DB_PASSWORD'),
-            'database': os.getenv('DB_NAME'),
+            "host": os.getenv("DB_HOST"),
+            "port": int(os.getenv("DB_PORT") or "5432"),
+            "user": os.getenv("DB_USER"),
+            "password": os.getenv("DB_PASSWORD"),
+            "database": os.getenv("DB_NAME"),
         }
-    
+
     async def get_connection(self) -> asyncpg.Connection:
         conn_params: dict = self.__parse_env_vars()
         return await asyncpg.connect(**conn_params)
