443eac4ea15a53e996b1b49afbf9feaca8b15abb
spuerta <puertasantiago2002@gmail.com>
2025-10-10 23:20:35 -0500

    style: Improved file style with pre-commit

--- src/register_ticket_api/interfaces/i_ticket_repository.py
+++ src/register_ticket_api/interfaces/i_ticket_repository.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from collections.abc import Awaitable
 from uuid import UUID
 
 from entities import Ticket, User
@@ -7,14 +6,14 @@ from entities import Ticket, User
 
 class ITicketRepository(ABC):
     @abstractmethod
-    async def register_ticket(self, user: User, ticket: Ticket) -> Awaitable[bool]:
+    async def register_ticket(self, user: User, ticket: Ticket) -> bool:
         pass
 
     @abstractmethod
-    async def get_by_ticket_details(self, seat: str, gate: str) -> Awaitable[Ticket | None]:
+    async def get_by_ticket_details(self, seat: str, gate: str) -> Ticket | None:
         # TODO: In the long run this will fail due to the abscence of an event entity
         pass
 
     @abstractmethod
-    async def mark_ticket_as_used(self, ticket_id: UUID) -> Awaitable[bool]:
+    async def mark_ticket_as_used(self, ticket_id: UUID) -> bool:
         pass
--- src/register_ticket_api/interfaces/i_user_repository.py
+++ src/register_ticket_api/interfaces/i_user_repository.py
@@ -1,14 +1,13 @@
 from abc import ABC, abstractmethod
-from collections.abc import Awaitable
 
 from entities import User
 
 
 class IUserRepository(ABC):
     @abstractmethod
-    async def create_user(self, new_user: User) -> Awaitable[bool]:
+    async def create_user(self, new_user: User) -> bool:
         pass
 
     @abstractmethod
-    async def get_by_username(self, username: str) -> Awaitable[User | None]:
+    async def get_by_username(self, username: str) -> User | None:
         pass
