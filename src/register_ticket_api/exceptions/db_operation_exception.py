e74c62d782cb3bb4385e1837058022f0a6d141e7
spuerta <puertasantiago2002@gmail.com>
2025-10-10 23:38:22 -0500

    style: Improved file style with pre-commit

--- src/register_ticket_api/exceptions/db_operation_exception.py
+++ src/register_ticket_api/exceptions/db_operation_exception.py
@@ -1,4 +1,5 @@
 class DbOperationException(Exception):
-    def __init__(self, message: str):
-        self.message = message
-        super().__init__(self.message)
+    def __init__(self, original_exception: Exception):
+        message = f"Error obtaining database connection: {original_exception}"
+        super().__init__(message)
+        self.original_exception = original_exception
