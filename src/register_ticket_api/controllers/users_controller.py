f73d973dad439cea2533beec7c3d00c23cfcf9ab
spuerta <puertasantiago2002@gmail.com>
2025-10-13 12:27:41 -0500

    style: Improved file style with pre-commit

--- src/register_ticket_api/controllers/users_controller.py
+++ src/register_ticket_api/controllers/users_controller.py
@@ -1,22 +1,21 @@
-from fastapi import APIRouter, HTTPException, status
-
-from services import UserService
 from entities import User
+from fastapi import APIRouter, HTTPException, status
+from services import UserService
+
 
 class UsersController:
-    def __init__(self, user_service: UserService):
+    def __init__(self, user_service: UserService) -> None:
         self.__user_service = user_service
         self.router = APIRouter(prefix="/api/users")
         self.__setup_routes()
-    
-    def __setup_routes() -> None:
-        ...
 
-    async def create_user(self, user: User):
+    def __setup_routes(self) -> None: ...
+
+    async def create_user(self, user: User) -> User:
         try:
-            self.__user_service.create_user(user)
+            return await self.__user_service.create_user(user)
         except Exception as err:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=f"Internal error: {str(err)}"
+                detail=f"Internal error: {err!s}",
             ) from err
