0093297f14e25c64e35753a0c4c3021bd837c5c0
spuerta <puertasantiago2002@gmail.com>
2025-10-13 12:24:26 -0500

    style: Improved file style with pre-commit

--- src/register_ticket_api/controllers/tickets_controller.py
+++ src/register_ticket_api/controllers/tickets_controller.py
@@ -1,24 +1,24 @@
+from entities import AttendanceLog, Ticket
+from exceptions import AppValidationException
 from fastapi import APIRouter, HTTPException, status
 from loguru import logger
-
 from services import TicketService
-from entities import Ticket, AttendanceLog
-from exceptions import AppValidationException
+
 
 class TicketsController:
     def __init__(self, ticket_service: TicketService):
         self.__ticket_service = ticket_service
         self.router = APIRouter(prefix="/api/users")
         self.__setup_routes()
-    
+
     def __setup_routes(self) -> None:
         self.router.add_api_route(
             "/{username}/tickets",
             self.register_ticket,
             methods=["POST"],
-            response_model= Ticket,
+            response_model=Ticket,
             status_code=status.HTTP_202_ACCEPTED,
-            summary= "Registers a ticket to a user"
+            summary="Registers a ticket to a user",
         )
         self.router.add_api_route(
             "/attendance",
@@ -26,24 +26,32 @@ class TicketsController:
             methods=["POST"],
             response_model=Ticket,
             status_code=status.HTTP_202_ACCEPTED,
-            summary= "Validates ticket via TOTP code"
+            summary="Validates ticket via TOTP code",
         )
 
-    async def register_ticket(self, username: str, ticket: Ticket):  # TODO: Change for user_id
-        logger.info(f"Request received at /tickets for username={username}, seat={ticket.seat}, gate={ticket.gate}")
+    async def register_ticket(
+        self, username: str, ticket: Ticket
+    ) -> Ticket:  # TODO: Change for user_id
+        logger.info(
+            f"Request received at /tickets for username={username}, "
+            f"seat={ticket.seat}, gate={ticket.gate}"
+        )
         try:
             return await self.__ticket_service.register_ticket(username, ticket)
         except AppValidationException as err:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err)) from err
         except Exception as err:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=f"Internal error: {str(err)}"
+                detail=f"Internal error: {err!s}",
             ) from err
 
     async def log_attendance(self, attendance: AttendanceLog) -> Ticket:
-        logger.info(f"Request received at /tickets for seat={attendance.seat}, gate={attendance.gate}, totp={attendance.totp_code}")
+        logger.info(
+            f"Request received at /tickets for seat={attendance.seat}, "
+            f"gate={attendance.gate}, totp={attendance.totp_code}"
+        )
         try:
             return await self.__ticket_service.log_attendance(attendance)
         except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+            raise HTTPException(status_code=500, detail=str(e)) from e
