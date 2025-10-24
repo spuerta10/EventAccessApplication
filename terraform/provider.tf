d51a1caafed1b095d7375d716ea356a1c7b558ad
spuerta <puertasantiago2002@gmail.com>
2025-10-23 20:46:32 -0500

    feat: Added backend to store infra state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  backend "gcs" {
+    bucket = "event-access-tfstate"
+    prefix = "terraform/state"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
